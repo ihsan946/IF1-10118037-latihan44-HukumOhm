88460769fd3810b21cf421fd55947f9dc4cb5ec6
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-10-20 10:29:08 +0700

    Add files via upload

---
+++ IF1-10118037-latihan44-HukumOhm/src/Main/Baterai.java
@@ -0,0 +1,50 @@
+package Main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Ihsan
+ */
+public class Baterai {
+    
+    private float kuatArus;
+    private float hambatan;
+    
+    public Baterai(){
+        System.out.println("=====Hukum Ohm=====");
+        System.out.println("Kuat arus yang mengalir pada suatu kawat penghantar");
+        System.out.println("akan berbanding lurus dengan beda potensial ");
+        System.out.println("pada ujung-ujung kawat penghantar tersebut");
+        System.out.println("asalkan suhu kawat dijaga konstan."); 
+               
+    }
+    public Baterai(float kuatArus, float hambatan){
+        this.kuatArus = kuatArus;
+        this.hambatan = hambatan;
+ 
+        System.out.println("\nKuat Arus : " + kuatArus + " ampere");
+        System.out.println("Hambatan : " +hambatan + " ohm" );
+        System.out.println("Hasil Tegangan : " + hitungTegangan() + " volt");
+        
+    
+    }
+
+    public float getKuatArus() {
+        return kuatArus;
+    }
+
+    public float getHambatan() {
+        return hambatan;
+    }
+    
+    public float hitungTegangan(){
+         return kuatArus * hambatan;
+    }
+    
+    
+}
---
+++ IF1-10118037-latihan44-HukumOhm/src/Main/main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Muhammad Ihsan
+ * NAMA       : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM          : 10118037
+ * Deskripsi Program   : Program ini berisi program perhitungan hukum Ohm
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Baterai batre = new  Baterai();
+        
+        Baterai batre2 = new Baterai(3, 12);
+        
+        
+        
+    }
+    
+}
